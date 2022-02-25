ed393c2ed92c0c7424a8a7d7e96266b9a55135df
FlutLab.io <>
2022-02-25 07:45:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,39 @@ class MyApp extends StatelessWidget {
             title: Text('GoWIST'),
             backgroundColor: Colors.blueAccent,
           ),
+          backgroundColor: Colors.blueGrey,
+          bottomNavigationBar: BottomNavigationGoWIST(),
         ));
+  }
+}
+
+class BottomNavigationGoWIST extends StatefulWidget {
+  @override
+  _BottomNavigationGoWISTState createState() => _BottomNavigationGoWISTState();
+}
+
+class _BottomNavigationGoWISTState extends State<BottomNavigationGoWIST> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        boxShadow: [
+          BoxShadow(color: Colors.grey.withOpacity(0.3), spreadRadius: 2, blurRadius: 15, offset: Offset(0, 5))
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
+        ),
+      ),
+      child: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: _selectedIndex == 0 ? new Icon(Icons.home_filled) : new Icon(Icons.home_outlined))
+        ],
+      ),
+    );
   }
 }
